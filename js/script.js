f42729d9760a7539ffb5bdbe1756fc8f8114e287
thisantana <thiago.fdsantana@gmail.com>
2017-09-22 01:25:40 -0300

    Primeiro commit com os arquivos base do projeto

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//FUNÇOES DO CONTROLADOR DO ORÇAMENTO
+var controladorOrcamento = (function() {
+    
+   //Adicionar codigo
+
+})()
+
+//FUNÇÂO DO CONTROLADOR DA INTERFACE
+var controladorInterface = (function(){
+
+    //Adicionar codigo
+
+})();
+
+//FUNÇÃO DO CONTROLADOR DO SISTEMA
+var controladorSistema = (function(cntrlOrcamento, cntrlInterface) {
+
+    //Adicionar códico
+     
+})(controladorOrcamento, controladorInterface)
